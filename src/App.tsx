26538b69dee803a697e8a3f3df04658163bb2308
Chris Kuhar <ckuhar@talageins.com>
2024-10-17 15:11:02 -0700

    Amplify changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-
-const client = generateClient<Schema>();
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={reactLogo} className="logo react" alt="React logo"/>
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+                <h1>Hello from Amplify</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
